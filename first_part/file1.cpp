4c01b38556975ca0ef42d4e9d8f18d085aaa77ce
BrunoBReis <brunobragancadosreis@gmail.com>
2023-10-05 09:15:36 -0300

    Adding new functions

--- first_part/file1.cpp
+++ first_part/file1.cpp
@@ -3,6 +3,7 @@
 #include <map>     // dictionary library
 #include <vector>  // dinamic list library
 #include <fstream> // files library
+#include <sstream> //
 using namespace std;
 
 // Global variables
@@ -12,12 +13,22 @@ const string file_name = "song.txt";
 vector<string> read_file(string file_name);
 void show_vector(vector<string> vector);
 vector<string> word_breaker(vector<string> vector);
+void tokenize(const string &str, const char delim, vector<string> &list);
+vector<string> final_list(vector<string> list);
+string upper_string(string word);
+bool word_verifier(string word, string word_to_verify);
+bool verify_word_in_database(vector<string> name_list, string new_word);
 
 int main()
 {
     vector<string> list_words = read_file(file_name);
-    // show_vector(list_words);
-    word_breaker(list_words);
+    show_vector(list_words);
+    cout << "----------------" << endl;
+    vector<string> breaked_list = word_breaker(list_words);
+    show_vector(breaked_list);
+    cout << "----------------" << endl;
+    vector<string> new_list = final_list(breaked_list);
+    show_vector(new_list);
 
     return 0;
 }
@@ -49,7 +60,7 @@ void show_vector(vector<string> vector)
 {
     for (int i = 0; i < vector.size(); i++)
     {
-        cout << vector[i] << endl;
+        cout << i << " " << vector[i] << endl;
     }
 }
 
@@ -58,9 +69,101 @@ vector<string> word_breaker(vector<string> list)
     vector<string> new_list;
     for (int i = 0; i < list.size(); i++)
     {
-        string name;
-        name = list[i];
-        cout << i << name << endl;
+        tokenize(list[i], ' ', new_list);
     }
     return new_list;
+}
+
+void tokenize(const string &str, const char delim, vector<string> &list)
+{
+    stringstream ss(str);
+    string s;
+
+    while (getline(ss, s, delim))
+    {
+        // tratar a virgula aqui
+        list.push_back(s);
+    }
+}
+
+vector<string> final_list(vector<string> list)
+{
+    vector<string> new_list;
+    for (int i = 0; i < list.size(); i++)
+    {
+        if (verify_word_in_database(new_list, list[i]))
+        {
+            if (list[i].size() > 3)
+            {
+                // for (int j = 0; j < list[i].size(); j++)
+                // {
+                //     if (list[i][j] == ',')
+                //     {
+                //         break;
+                //     }
+                //     new_list.push_back(list[i]);
+                // }
+                new_list.push_back(list[i]);
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        continue;
+    }
+
+    return new_list;
+}
+
+string upper_string(string word)
+{
+    for (int i = 0; i < word.length(); i++)
+    {
+        word[i] = toupper(word[i]);
+    }
+
+    return word;
+}
+
+bool word_verifier(string word, string word_to_verify)
+{
+    word = upper_string(word);
+    word_to_verify = upper_string(word_to_verify);
+
+    if (word.length() == word_to_verify.length())
+    {
+        int words_lenght = word.length();
+        int tries;
+        for (int i = 0; i < words_lenght; i++)
+        {
+            if (word[i] != word_to_verify[i])
+            {
+                return false;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool verify_word_in_database(vector<string> name_list, string new_word)
+{
+
+    for (int i = 0; i < name_list.size(); i++)
+    {
+        if (word_verifier(new_word, name_list[i]))
+        {
+            return false;
+        }
+    }
+    return true;
 }
